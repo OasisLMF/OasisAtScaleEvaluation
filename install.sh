459d36e13dc57eb2c4fe4c616ac73b706e5ada2e
Dan Bate <dan.bate@gmail.com>
2020-04-03 11:19:18 +0100

    initial worker controller integration

--- install.sh
+++ install.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 set -e
+set -x
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-export VERS_API=1.6.0
-export VERS_WORKER=1.6.0
+export VERS_API=arch-2020-alpha
+export VERS_WORKER=arch-2020-alpha
 export VERS_UI=1.5.1
 export VERS_PIWIND=1.6.0
+export VERS_WORKER_CONTROLLER=latest
 GIT_UI=OasisUI
 GIT_API=OasisPlatform
+GIT_API_BRANCH=architecture-2020
 GIT_PIWIND=OasisPiWind
+GIT_WORKER_CONTROLLER=OasisWorkerController
+GIT_WORKER_CONTROLLER_BRANCH=master
 
 
 # ---  OASIS UI --- # 
@@ -27,12 +32,12 @@ fi
 if [ -d $SCRIPT_DIR/$GIT_API ]; then
     cd $SCRIPT_DIR/$GIT_API
     git stash
-    git fetch && git checkout $VERS_API
+    git fetch && git checkout $GIT_API_BRANCH
 else
     mkdir -p $SCRIPT_DIR/$GIT_API
     cd $SCRIPT_DIR/$GIT_API
     git clone https://github.com/OasisLMF/$GIT_API.git .
-    git checkout $VERS_API
+    git checkout $GIT_API_BRANCH
 fi 
 
 # ---  MODEL PiWind --- # 
@@ -45,8 +50,19 @@ else
     cd $SCRIPT_DIR/$GIT_PIWIND
     git clone https://github.com/OasisLMF/$GIT_PIWIND.git .
     git checkout $VERS_PIWIND
-fi 
+fi
 
+# ---  OASIS Worker Controller --- #
+if [ -d $SCRIPT_DIR/$GIT_WORKER_CONTROLLER ]; then
+    cd $SCRIPT_DIR/$GIT_WORKER_CONTROLLER
+    git stash
+    git fetch && git checkout $GIT_WORKER_CONTROLLER_BRANCH
+else
+    mkdir -p $SCRIPT_DIR/$GIT_WORKER_CONTROLLER
+    cd $SCRIPT_DIR/$GIT_WORKER_CONTROLLER
+    git clone https://github.com/OasisLMF/$GIT_WORKER_CONTROLLER.git .
+    git checkout $GIT_WORKER_CONTROLLER_BRANCH
+fi
 
 
 # setup and run API
@@ -59,6 +75,20 @@ sed -i "s|:latest|:${VERS_API}|g" docker-compose.yml
 set +e
 docker-compose down
 docker-compose pull
+set -e
+docker-compose up -d --no-build worker-monitor channel-layer celery-beat task-controller server
+
+# setup and run the worker controller
+cd $SCRIPT_DIR/$GIT_WORKER_CONTROLLER
+export OASIS_MODEL_DATA_DIR=$SCRIPT_DIR/$GIT_PIWIND
+export OASIS_MEDIA_ROOT=$SCRIPT_DIR/$GIT_API/docker-shared-fs/
+export OASIS_MODEL_WORKER_VERSION=$VERS_WORKER
+git checkout -- docker-compose.yaml
+sed -i "s|coreoasis/worker-controller:latest|coreoasis/worker-controller:${VERS_WORKER_CONTROLLER}|g" docker-compose.yaml
+
+set +e
+docker-compose down
+#docker-compose pull
 set -e
 docker-compose up -d --no-build
 
